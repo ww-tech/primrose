97d1ca464cbc1f13c55c2de01aa01f3a5fcc1021
Kevin Zecchini <kevin.zecchini@weightwatchers.com>
2019-11-27 15:41:05 -0500

    updating git push to after the bump script

--- scripts/git_push.sh
+++ scripts/git_push.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
-  # Remove existing "origin"
+push_commit() {
   git remote rm origin
   # Add new "origin" with access token in the git URL for authentication
   git remote add origin https://$GITHUB_PERSONAL_ACCESS_TOKEN@github.com:ww-tech/primrose.git > /dev/null 2>&1
+  git push origin master --tags --quiet
 }
 
-setup_git
+push_commit
