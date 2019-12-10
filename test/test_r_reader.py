21d3c461e36e576e05674b6a484046278a43a42d
Carl Anderson <carl.anderson@weightwatchers.com>
2019-12-10 12:36:27 -0500

    initial commit

---
+++ test/test_r_reader.py
@@ -0,0 +1,34 @@
+
+import pytest
+import sys
+import os
+import pandas as pd
+from primrose.configuration.configuration import Configuration
+from primrose.readers.r_reader import RReader
+from primrose.node_factory import NodeFactory
+from primrose.data_object import DataObject
+from primrose.base.node import AbstractNode
+from primrose.data_object import DataObject, DataObjectResponseType
+
+def test_run():
+    config = {
+        "implementation_config": {
+            "reader_config": {
+                "read_data": {
+                    "class": "RReader",
+                    "dataset": "iris",
+                    "destinations": []
+                }
+            }
+        }
+    }
+    configuration = Configuration(config_location=None, is_dict_config=True, dict_config=config)
+    data_object = DataObject(configuration)
+
+    reader = RReader(configuration, "read_data")
+    data_object, terminate = reader.run(data_object)
+    assert not terminate
+    df = data_object.get('read_data', rtype=DataObjectResponseType.VALUE.value)
+    assert df is not None
+    assert df.shape == (150, 6)
+    assert list(df.columns) == ["Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width", "Species", "row_names"]
