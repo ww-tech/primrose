4a13dcedc3a2b5fcca8e0bdee4a6269b7f5b6eff
Kevin Zecchini <kevin.zecchini@weightwatchers.com>
2019-12-19 16:55:18 -0500

    fixing commit push function

--- scripts/bump_script.sh
+++ scripts/bump_script.sh
@@ -3,8 +3,8 @@
 push_commit() {
   git remote rm origin
   # Add new "origin" with access token in the git URL for authentication
-  git remote add origin https://$GITHUB_PERSONAL_ACCESS_TOKEN@github.com:ww-tech/primrose.git > /dev/null 2>&1
-  git push origin master --tags --quiet
+  git remote add origin https://$GITHUB_PERSONAL_ACCESS_TOKEN@github.com/ww-tech/primrose.git > /dev/null 2>&1
+  git push origin $TRAVIS_BRANCH --tags --quiet
 }
 
 # use git tag to trigger a build and decide how to increment
