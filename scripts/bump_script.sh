fed7103e2d914b64fe29047784e7e00769e23a85
Kevin Zecchini <kevin.zecchini@weightwatchers.com>
2020-02-07 17:50:32 -0500

    updating bump script

--- scripts/bump_script.sh
+++ scripts/bump_script.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 push_commit() {
-  git remote rm origin
-  # Add new "origin" with access token in the git URL for authentication
-  echo `git branch`
-  echo `git status --porcelain`
-  git remote add origin https://$GITHUB_PERSONAL_ACCESS_TOKEN@github.com/ww-tech/primrose.git > /dev/null 2>&1
-  git push origin HEAD:$TRAVIS_BRANCH --quiet && git push origin HEAD:$TRAVIS_BRANCH --tags --quiet
+    echo `git branch`
+    echo ""
+    echo `git status --porcelain`
+
+    git remote rm origin
+    # Add new "origin" with access token in the git URL for authentication
+    git remote add origin https://$GITHUB_PERSONAL_ACCESS_TOKEN@github.com/ww-tech/primrose.git > /dev/null 2>&1
+    git fetch
+    git push origin HEAD:$TRAVIS_BRANCH --quiet && git push origin HEAD:$TRAVIS_BRANCH --tags --quiet
 }
 
 # use git tag to trigger a build and decide how to increment
