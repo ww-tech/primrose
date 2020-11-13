095fe9275fb2d91ba13dc18fe208dcaa5675239a
Travis CI <travis@travis-ci.org>
2020-11-13 20:59:00 +0000

    [skip travis] Bump version: 1.0.12 -> 1.0.13-dev

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ local_config_files = [os.path.join("config", f) for f in os.listdir("config")]
 
 setup(
     name="primrose",
-    version="1.0.12",
+    version="1.0.13-dev",
     description="Primrose: a framework for simple, quick modeling deployments",
     url="https://github.com/ww-tech/primrose",
     author="Carl Anderson",
