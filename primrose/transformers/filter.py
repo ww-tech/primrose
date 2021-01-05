b19b5587b9baa86eff89f6db727920abb3f00cba
Daisy Leung <daisy.leung@ww.com>
2021-01-04 19:13:50 -0500

    added FilterByUnivariateQuantile

--- primrose/transformers/filter.py
+++ primrose/transformers/filter.py
@@ -1,4 +1,4 @@
-"""Transform data by filtering in data using filtering operations 
+"""Transform data by filtering in data using filtering operations
 
 Author(s):
     Michael Skarlinski (michael.skarlinski@weightwatchers.com)
@@ -10,6 +10,8 @@ import operator
 import logging
 import numpy as np
 import pandas as pd
+import itertools
+
 
 from primrose.base.transformer import AbstractTransformer
 
@@ -94,3 +96,92 @@ class FilterByPandasExpression(AbstractTransformer):
             logging.info("Filtered out %d rows" % (len(data) - len(filtered_data)))
 
         return filtered_data
+
+
+class FilterByUnivariateQuantile(AbstractTransformer):
+    def __init__(self, features_to_filter, multiplier=2.0, verbose=False):
+        """
+
+        features_to_filter: list of str
+            cols to perform filtering on
+
+        """
+        self.features_to_filter = features_to_filter
+        self.multiplier = multiplier
+        self.verbose = verbose
+
+
+    def fit(self, data):
+        """fit data, here just passing
+
+        Args:
+            data (object): some data
+
+        """
+        pass
+
+
+    def remove_univariate_outliers(self, df, xvarlist, multiplier=2.0, verbose=False):
+
+        """
+            Remove outlier, defined as 2.0x interquartile range (IQR) of a single variable.
+
+        Parameters
+        ----------
+            xvarlist: list of str
+                colnames
+        """
+
+        df = df.dropna(inplace=False)
+        df = df.reset_index(drop=True, inplace=False)
+        outliers_lst = []
+
+        for feature in xvarlist:
+            Q3 = np.percentile(df[feature], 75)
+            Q1 = np.percentile(df[feature], 25)
+            step = multiplier * (Q3 - Q1)
+            outliers_rows = df.loc[~((df[feature] >= Q1 - step) & (df[feature] <= Q3 + step)), :]
+            outliers_lst.append(list(outliers_rows.index))
+            if verbose:
+                print(feature)
+                print(Q1, Q3)
+                print(step)
+                print(len(outliers_rows))
+        outliers = list(itertools.chain.from_iterable(outliers_lst))
+        uniq_outliers = list(set(outliers))
+
+        if verbose:
+            print('Length of outliers list:\n', len(uniq_outliers))
+        good_data = df.drop(df.index[uniq_outliers]).reset_index(drop=True)
+        return good_data
+
+
+    def transform(self, data):
+        """
+
+        Args:
+            data (dict): dictionary with dataframes from all readers
+
+        Returns:
+            dataframe with filtered data
+
+        Raises:
+            Exception if not a pandas dataframe, operation not supported, or column name not recognized
+
+        """
+        if not isinstance(data, pd.DataFrame):
+            raise Exception("Data is not a pandas DataFrame")
+
+        if len(self.features_to_filter) == 0:
+            logging.info("no filters found; returning combined_data as filtered_data")
+            return data
+
+        else:
+
+            filtered_data = self.remove_univariate_outliers(data, self.features_to_filter, multiplier=self.multiplier, verbose=self.verbose)
+
+            logging.info("Filtered out %d rows" % (len(data) - len(filtered_data)))
+
+        return filtered_data
+
+
