1cd33098e4990bb563ddd1bbc152078000e95ceb
Daisy Leung <daisy.leung@ww.com>
2021-01-04 19:30:58 -0500

    added test

--- test/test_transformer_filters.py
+++ test/test_transformer_filters.py
@@ -1,7 +1,7 @@
 import pytest
 import pandas as pd
 
-from primrose.transformers.filter import FilterByPandasExpression
+from primrose.transformers.filter import FilterByPandasExpression, FilterByUnivariateQuantile
 
 
 def test_transform():
@@ -51,3 +51,12 @@ def test_transform5():
     with pytest.raises(Exception) as e:
         ft.transform(df)
     assert "Unsupported filter operation 'JUNK'" in str(e)
+
+
+
+def test_univariate_transform():
+
+    df = pd.DataFrame([[1000],[2000],[3000],[4000],[5000],[6000],[7000],[8000],[9000],[20000], [1e6], [-1e5]], columns=['tmp'])
+
+    good_data = FilterByUnivariateQuantile(features_to_filter=['tmp'], multiplier=2.0).transform(df)
+    assert good_data.shape == (9, 1)
