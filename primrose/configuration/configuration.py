a66ce76e49409f99cacebcbfce04303c0672002e
Parul L <parul.laul@gmail.com>
2022-09-19 14:37:52 -0400

    Cleaned up exc error

--- primrose/configuration/configuration.py
+++ primrose/configuration/configuration.py
@@ -381,7 +381,7 @@ class Configuration:
                     logging.info(f"attempting to register {class_key}")
                     self._register_class(class_key, class_prefix)
                 except Exception as exc:
-                    logging.error(f"Cannot register node class {class_key} with prefix {class_prefix}: \n {exc}")
+                    logging.error(f"Cannot register node class {class_key} with prefix {class_prefix}", exc_info=exc)
         for class_key in unique_nodes:
             if not NodeFactory().is_registered(class_key):
                 raise ConfigurationError(f"Cannot register node class {class_key}")
