7c58bafab789eecfbb0153a771a4de16cf52a823
calvin d. woo <calvin.woo@ww.com>
2023-03-22 18:41:16 -0400

    remove failing package install in linux bootstrap
    bump test

--- scripts/linux_install.sh
+++ scripts/linux_install.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 echo "Installing linux packages"
-sudo apt-get install python-dev libgraphviz-dev pkg-config graphviz
+sudo apt-get update
+sudo apt-get install libgraphviz-dev pkg-config graphviz
+dpkg -L libgraphviz-dev
 echo "Updating matplotlib configuration"
 mkdir -p ~/.config/matplotlib && touch ~/.config/matplotlib/matplotlibrc
 echo "backend: Agg" >> ~/.config/matplotlib/matplotlibrc
--- scripts/mac_install.sh
+++ scripts/mac_install.sh
@@ -6,3 +6,4 @@ brew reinstall openssl
 echo "Updating matplotlib configuration"
 mkdir -p ~/.matplotlib && touch ~/.matplotlib/matplotlibrc
 echo "backend: TkAgg" >> ~/.matplotlib/matplotlibrc
+
