d3c665c61b7c87459d76a230251a2c6b96d06c93
calvin d. woo <calvin.woo@ww.com>
2023-03-23 15:41:20 -0400

    napolean is upgraded to a sphinx extension

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -54,7 +54,7 @@ release = "1.0.0"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinxcontrib.napoleon",
+    "sphinx.ext.napoleon",
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "recommonmark",
